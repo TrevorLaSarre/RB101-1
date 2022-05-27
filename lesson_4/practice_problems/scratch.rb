a00fce01fe3edffad8546c71ff73e3fe5eb331b9
JD Fortune <jondfortune@gmail.com>
2022-05-27 17:47:42 -0500

    ok

--- lesson_4/practice_problems/scratch.rb
+++ lesson_4/practice_problems/scratch.rb
@@ -53,24 +53,60 @@
 
 # p general_select(produce_list)
 
-alphabet = ('a'..'z').to_a
+# alphabet = ('a'..'z').to_a
 
-question = 'How many times does a particular character appear in this sentence?'
+# question = 'How many times does a particular character appear in this sentence?'
 
-def select_letter(alphabet, letter = 'g')
-  counter = 0
-  selected_letter = ''
+# def select_letter(alphabet, letter = 'g')
+#   counter = 0
+#   selected_letter = ''
 
-  loop do
-    break selected_letter if counter == alphabet.size
+#   loop do
+#     break selected_letter if counter == alphabet.size
 
-    current_letter = alphabet[counter]
-    if current_letter == letter
-      selected_letter << current_letter
-    end
+#     current_letter = alphabet[counter]
+#     if current_letter == letter
+#       selected_letter << current_letter
+#     end
 
-    counter += 1
+#     counter += 1
+#   end
+# end
+
+# p select_letter(question, 'a').size
+
+# words = "the flintstones rock"
+# p words
+# counter = 0
+
+# loop do
+#   if counter == 0 || words[counter - 1] == ' '
+#     words = words[0...counter] + words[counter].capitalize + words[counter + 1..-1]
+#   end
+#   counter += 1
+#   break words if counter == words.size
+# end
+
+# p words
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, hash|
+  age = hash["age"]
+  case age
+  when (0..17)
+    hash['age_group'] = 'kid'
+  when (18..64)
+    hash['age_group'] = 'adult'
+  else
+    hash['age_group'] = 'senior'
   end
 end
 
-p select_letter(question, 'a').size
+puts munsters
